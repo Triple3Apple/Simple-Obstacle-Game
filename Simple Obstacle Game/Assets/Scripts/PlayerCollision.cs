204dfa9803368ff9646c95c6ab2eba75b77b41d2
Abrar Akhand <Jahin345@gmail.com>
2020-08-09 23:13:06 -0700

    Finished level 1

--- Simple Obstacle Game/Assets/Scripts/PlayerCollision.cs
+++ Simple Obstacle Game/Assets/Scripts/PlayerCollision.cs
@@ -34,8 +34,23 @@ public class PlayerCollision : MonoBehaviour
             
             //FindObjectOfType<GameManager>().EndGame();
         }
+    }
 
+    public void killPlayer()
+    {
+        Debug.Log("Obstacle Hit");
 
+        _playerMoveScript.enabled = false;   // when you hit an obstacle, movement is turned off
 
+        // enable rotation on y and z axis when player hits a obstacle
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.None;
+
+        // only end game if level is not completed (this prevents level from restarting when player finishes game and then hits a obstacle)
+        GameManager gameManager = FindObjectOfType<GameManager>();
+        if (!gameManager.isLevelCompleted)
+        {
+            gameManager.EndGame();
+        }
     }
 }
---
+++ Simple Obstacle Game/Assets/Scripts/TumbleweedCollision.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TumbleweedCollision : MonoBehaviour
+{
+
+    ParticleSystem particleSystem;
+    List<ParticleCollisionEvent> particleCollisionEvents = new List<ParticleCollisionEvent>();
+
+    [SerializeField] PlayerCollision playerCollisionScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        /*
+        int numCollisionEvents = particleSystem.GetCollisionEvents(other, particleCollisionEvents);
+
+        for (int i = 0; i < numCollisionEvents; i++)
+        {
+            if (other.CompareTag("Player"))
+            {
+                playerCollisionScript.killPlayer();
+            }
+            
+        }
+        */
+
+        if (other.CompareTag("Player"))
+        {
+            playerCollisionScript.killPlayer();
+        }
+    }
+}
